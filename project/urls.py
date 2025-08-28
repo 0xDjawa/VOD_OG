1342775704c7a612ffc7988b40d9ad32236face9
0xDjawa <artificialjawa@gmail.com>
2025-08-28 16:20:55 +0700

    Update video processing and VOD integration

--- project/urls.py
+++ project/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('video.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Add this line if not present
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
